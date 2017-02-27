9dcce672aeff4849e5eebafca8bd24722ad43660
hanchun <xdhanchun@gmail.com>
2017-02-27 14:22:46 +0800

    Main.java 和 master.java中增加say()

--- Main.java
+++ Main.java
@@ -3,5 +3,7 @@ public class Main{
   public static void main(String[] args) {
 
   }
+ 
+  public void hello(){}
 
 }
--- master.java
+++ master.java
@@ -2,6 +2,7 @@ public class master{
 
 public static void main(String[] args) {
 int i = 0;
+public void say(){}
 
 }
 
