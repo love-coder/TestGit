f9389bffc2724e9333b93027532dccf1dbc96e04
hanchun <xdhanchun@gmail.com>
2017-02-27 18:28:45 +0800

    新建Hello.java

---
+++ Hello.java
@@ -0,0 +1,25 @@
+public class Hello{
+
+
+  public void say() {
+    System.out.println("你好！");
+  }
+        public void say() {
+    System.out.println("你好！");
+  }public void say() {
+    System.out.println("你好！");
+  }public void say() {
+    System.out.println("你好！");
+  }public void say() {
+    System.out.println("你好！");
+  }public void say() {
+    System.out.println("你好！");
+  }public void say() {
+    System.out.println("你好！");
+  }public void say() {
+    System.out.println("你好！");
+  }public void say() {
+    System.out.println("你好！");
+  }
+
+}
